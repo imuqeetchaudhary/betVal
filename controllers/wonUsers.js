f15ea7940236503b86ffb18655aacebabbf05dbf
Abdul Muqeet <abduliscoool@gmail.com>
2021-06-10 15:54:13 +0500

    implementation to get all won users

--- controllers/wonUsers.js
+++ controllers/wonUsers.js
@@ -1,7 +1,26 @@
 const { UserBetslip } = require("../db/models/userBetslip")
 const { MatchResult } = require("../db/models/matchResult")
+const { WonUser } = require("../db/models/wonUser")
 
 exports.getWonUsers = async (req, res) => {
+    try {
+        const wonUsers = await WonUser.find({matchId: req.body.matchId})
+        if(!wonUsers) return res.status(404).json({
+            message: "Not found any winning user"
+        })
+
+        res.status(200).json({
+            wonUsers: wonUsers
+        })
+    }
+    catch (err) {
+        res.status(400).json({
+            message: `${err}`
+        })
+    }
+}
+
+exports.checkBetStatus = async (req, res) => {
     try {
         const matchResult = await MatchResult.findOne({ matchId: req.body.matchId })
         if (!matchResult) return res.status(404).json({
@@ -24,11 +43,29 @@ exports.getWonUsers = async (req, res) => {
                         betReturn: userBetslip.stake * userBetslip.totalOdds
                     }
                 })
+            
+            const wonUser = await WonUser.findOne({userId: req.user._id})
+            if(!wonUser) {
+                const newWonUser = new WonUser({
+                    userId: req.user._id,
+                    matchId: req.body.matchId,
+                    betReturn: userBetslip.stake * userBetslip.totalOdds
+                })
 
-            res.status(200).json({
-                message: "You won the bet",
-                betReturn: userBetslip.stake * userBetslip.totalOdds
-            })
+                await newWonUser.save()
+                res.status(200).json({
+                    message: "You won the bet",
+                    betReturn: userBetslip.stake * userBetslip.totalOdds
+                })
+            }
+            else {
+                res.status(200).json({
+                    message: "You won the bet",
+                    betReturn: userBetslip.stake * userBetslip.totalOdds
+                })
+            }
+
+            
         }
 
         if (matchResult.isHomeTeamWon == true && matchResult.isBtts == true && matchResult.isOver25 === true && userBetslip.homeOdd > 0 && userBetslip.bttsYes > 0 && userBetslip.over25 > 0) {
--- routes/wonUsers.js
+++ routes/wonUsers.js
@@ -2,7 +2,11 @@ const express = require("express")
 const router = express.Router()
 const wonUsers = require("../controllers/wonUsers")
 const { authentication } = require("../middlewares/isAuth")
+const { validation } = require("../middlewares/validation")
+const { wonUserSchema } = require("../validation/wonUser")
 
-router.get("/", authentication, wonUsers.getWonUsers)
+
+router.get("/", authentication, validation(wonUserSchema), wonUsers.getWonUsers)
+router.get("/get", authentication, validation(wonUserSchema), wonUsers.checkBetStatus)
 
 module.exports = router
